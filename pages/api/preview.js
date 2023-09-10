a2d80b076deaa10381a27a51bc7adb526b516941
domingosled <l.r.m.d@sapo.pt>
2023-09-10 06:38:20 +0100

    fix preview bugs

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,6 +1,8 @@
 import { getPageData } from "utils/api"
 import { parseCookies } from "utils/parse-cookies"
 
+//use i.e http://localhost:3000/api/preview?locale=pt&secret=execlog_preview&slug=contacto
+
 const preview = async (req, res) => {
   // Check the secret and next parameters
   // This secret should only be known to this API route and the CMS
@@ -11,12 +13,13 @@ const preview = async (req, res) => {
   const cookies = parseCookies(req)
   const slugArray = req.query.slug.split("/")
   // Fetch the headless CMS to check if the provided `slug` exists
+  let locale= req.query.locale
   const pageData = await getPageData({
     locale,
     slug: slugArray.join("/"),
     preview: true,
   })
-
+  console.log(pageData)
   // If the slug doesn't exist prevent preview mode from being enabled
   if (!pageData) {
     return res.status(401).json({ message: "Invalid slug" })
@@ -29,7 +32,7 @@ const preview = async (req, res) => {
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
   // Prefix with locale so previews are available in all languages
   res.writeHead(307, {
-    Location: `/${pageData.locale}/${pageData.slug}`,
+    Location: `/${pageData.attributes.locale}/${pageData.attributes.slug}`,
   })
   res.end()
 }
