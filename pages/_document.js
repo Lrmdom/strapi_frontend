9c3bbd53d369af1947087a05278824c26dd8aae7
domingosled <l.r.m.d@sapo.pt>
2023-09-25 12:54:29 +0100

    tagmanager activation

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,6 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDWZ9LZ7"
-                          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <Main />
           <NextScript />
         </body>
