404aecb431abca0da2c0b5f665e64d36c2b3bd3b
domingosled <l.r.m.d@sapo.pt>
2023-10-12 12:38:36 +0100

    problems solving

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const { colors } = require(`tailwindcss/defaultTheme`)
+const  colors  = require(`tailwindcss/colors`)
 
 module.exports = {
   mode: "jit", // see https://tailwindcss.com/docs/just-in-time-mode
