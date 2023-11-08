e83d5245649f48c4f1d6383766d12566c255b746
domingosled <l.r.m.d@sapo.pt>
2023-11-08 22:28:49 +0000

    problems solving

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,11 +8,11 @@ import InstagramProvider from "next-auth/providers/instagram"
 import AppleProvider from "next-auth/providers/apple"
 export const authOptions = {
   pages: {
-    signIn: '/auth/signin',
-    signOut: '/auth/signout',
-    error: '/auth/error', // Error code passed in query string as ?error=
-    verifyRequest: '/auth/verify-request', // (used for check email message)
-    newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+    signIn: '/api/auth/signin',
+    signOut: '/api/auth/signout',
+    error: '/api/auth/error', // Error code passed in query string as ?error=
+    verifyRequest: '/api//auth/verify-request', // (used for check email message)
+    newUser: '/api//auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
   },
   // Configure one or more authentication providers
   providers: [
@@ -49,4 +49,4 @@ export const authOptions = {
   ]
 }
 
-export const { handlers, auth, signIn, signOut } = NextAuthConfig(authOptions)
+export default NextAuth(authOptions)
