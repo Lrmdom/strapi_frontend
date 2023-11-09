97edf616ca9020e42d39fcff00f2a51fc7e346c6
domingosled <l.r.m.d@sapo.pt>
2023-11-09 17:20:36 +0000

    problems solving

--- pages/api/auth/signOut.js
+++ pages/api/auth/signOut.js
@@ -4,7 +4,6 @@ export default function SignOut({ providers }) {
 
     return (
       <>
-        Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
     )
